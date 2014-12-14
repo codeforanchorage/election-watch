1524d1e1cab1c04ead849f021d6f97d7701082ab
Geoffrey Humphreys <geoffrey.g.humphreys@gmail.com>
2014-12-14 12:17:19 -0500

    Updates to enhance readability

--- scripts/get_election_results.pl
+++ scripts/get_election_results.pl
@@ -5,10 +5,14 @@ use DB_File;
 
 my $DATA_DIRECTORY = "/var/tmp/";
 
+# Grab the data from the Division of Elections using a text mode web browser
 open IN, "lynx -dump 'http://www.elections.alaska.gov/results/14GENR/data/results.htm' |";
-#open IN, "lynx -width=320 -dump 'http://localhost/results.htm' |";
+
+# Open an output file for text messages
 open OUT, ">texts.txt";
 
+# Strip off some headers.
+# Note: These are not currently used for anything and can be renamed as required.
 my $ln_header1 = <IN>;
 my $ln_header2 = <IN>;
 my $ln_header3 = <IN>;
@@ -20,15 +24,23 @@ my $ln_header8 = <IN>;
 my $ln_header9 = <IN>;
 my $ln_header10 = <IN>;
 
+# Get the lock on the database
 dblock();
+
+# Open the database
 my %races;
 dbmopen(%races, "$DATA_DIRECTORY/races", 0666);
 my %results;
 dbmopen(%results, "$DATA_DIRECTORY/results", 0666);
+
+# Open the numbers hashtable
+# Contains: Subscriber phone number => list of subscribed races
 my %numbers;
 dbmopen(%numbers, "$DATA_DIRECTORY/numbers", 0666);
 
-my %racenumbers;
+# Read in the entire numbers hashtable and split/reverse it to race => number
+# so we can look up subscribers by race
+my %racenumbers;  # contains race => @subscriber_list pairs
 foreach my $number (keys %numbers)
 {
 	my $racestring = $numbers{$number};
@@ -44,6 +56,7 @@ foreach my $number (keys %numbers)
 	}
 }
 
+# Loop through all the races to the end of the page
 while(my $ln_race = <IN>)
 {
 	my $ln_total = <IN>;
@@ -54,21 +67,27 @@ while(my $ln_race = <IN>)
 	my $ln_line = <IN>;
 	my $ln_filler = <IN>;
 
+	# Parse race name
 	my $race = $ln_race;
+	# Remove trailing newline and leading/trailing whitespace
 	chomp $race;
 	$race =~ s/^ +//;
 	$race =~ s/ +$//g;
 
+	# Parse total precinct count
 	$ln_precincts =~ m/([0-9]+)/;
 	my $precincts = $1;
 
+	# Parse precinct reporting count
 	$ln_reporting =~ m/([0-9]+) ([0-9.]+)/;
 	my $reporting = $1;
 	my $pct_reporting = $2;
 
-	print "found race: $race $precincts/$reporting ($pct_reporting\%)\n";
+#	print STDERR "found race: $race $precincts/$reporting ($pct_reporting\%)\n";
+
+	# Check to see if the race has been updated
 	my $racestring = "$precincts $reporting $pct_reporting";
-	my $should_update_race;
+	my $should_update_race; # boolean flag stating whether race updated
 	my $text;
 	if($racestring ne $races{$race})
 	{
@@ -80,7 +99,9 @@ while(my $ln_race = <IN>)
 		$should_update_race = 0;
 	}
 
-	my $racetext = "";
+	# Parse the rest of the race and prepare a short report
+	# This step is performed even if the race has not been updated
+	my $racetext = ""; # a short report on the race
 	for(;;)
 	{
 		my $ln_res = <IN>;
@@ -97,16 +118,20 @@ while(my $ln_race = <IN>)
 		my $lname = $candidate;
 		$lname =~ s/[, ].*//;
 #		last if($candidate =~ m/MEASURE/);
-#		print "found candidate: $race\t$candidate\t$party\t$count\t$percentage\n";
+#		print STDERR "found candidate: $race\t$candidate\t$party\t$count\t$percentage\n";
 		if($should_update_race)
 		{
+			# Update the race hashtable
 			my $racekey = "$race-$candidate-$party";
 			my $raceval = "$percentage-$count";
 			$results{$racekey} = $raceval;
 		}
+
 		$racetext .= "$lname $party $percentage\%\n";
 	}
 	chomp $racetext;
+
+	# If the race has been updated, broadcast the report
 	if($should_update_race)
 	{
 		$racetext = "Election Watch\n$race\n($reporting/$precincts)\n$racetext";
